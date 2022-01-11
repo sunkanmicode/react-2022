2fd710af409c49151bf256a6260096eb9f853cce
Sunkanmi <sunkanmi.oguntimehin@optisoft.ng>
2022-01-10 16:54:25 -0800

    initial commit

---
+++ pdf-react/src/App.js
@@ -0,0 +1,13 @@
+import Example from "./pdfContent/PdfPage";
+import PdfPage from "./pdfContent/PdfPage";
+
+
+function App() {
+  return (
+    <div className="container mx-auto px-4">
+      <PdfPage />
+    </div>
+  );
+}
+
+export default App;
